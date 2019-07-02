58e8387ad3bcf00816728a76171c4cce5b34f479
Debian <admin@ip-172-26-5-77.ec2.internal>
2019-07-02 00:53:10 +0000

    rclone tar backup working

---
+++ rcloneBackup.sh
@@ -0,0 +1,14 @@
+echo "Backing up wordpress directory to google drive .."
+
+# 2018 08 27 fh: sync is needed, so that old files get delete on google drive
+
+mkdir /tmp/espiraldelser
+sudo tar cvf /tmp/espiraldelser/db-data.tar /home/admin/ranchokalanWP/espiraldelser/db-data
+sudo tar cvf /tmp/espiraldelser/wp-content.tar /home/admin/ranchokalanWP/espiraldelser/wp-content
+rclone sync /tmp/espiraldelser    flogoogle:rclone/espiraldelser/ --retries 5
+
+mkdir /tmp/ranchokalan
+sudo tar cvf /tmp/ranchokalan/db-data.tar /home/admin/ranchokalanWP/ranchokalan/db-data
+sudo tar cvf /tmp/ranchokalan/wp-content.tar /home/admin/ranchokalanWP/ranchokalan/wp-content
+rclone sync /tmp/ranchokalan     flogoogle:rclone/ranchokalan/ --retries 5
+
--- rcloneBackupRanchokalan.sh
+++
@@ -1,9 +0,0 @@
-
-
-echo "Backing up zitytripData directory to google drive .."
-
-# 2018 08 27 fh: sync is needed, so that old files get delete on google drive
-
-rclone sync /home/admin/ranchokalanWP/ranchokalan/db-data     flogoogle:rclone/ranchokalan/db-data --retries 5
-rclone sync /home/admin/ranchokalanWP/ranchokalan/wp-content  flogoogle:rclone/ranchokalan/wp-content --retries 5
-
